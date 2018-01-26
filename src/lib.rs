6854bbf77544973f39d1e9f8fc4bb8a94488d30a
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-01-27 01:12:42 +0900

    Import test of SFMT

--- src/lib.rs
+++ src/lib.rs
@@ -22,15 +22,6 @@ impl SFMT {
         unsafe { ffi::sfmt_gen_rand_all(&mut self.0 as *mut _) };
     }
 
-    fn increment(&mut self) {
-        if self.0.idx >= SFMT_N32 {
-            self.gen_all();
-            self.0.idx = 0;
-        } else {
-            self.0.idx += 1;
-        }
-    }
-
     fn get(&self) -> u32 {
         unsafe {
             let ptr = &self.0.state[0].u[0] as *const u32;
@@ -47,7 +38,38 @@ impl SFMT {
 
 impl Rng for SFMT {
     fn next_u32(&mut self) -> u32 {
-        self.increment();
-        self.get()
+        if self.0.idx >= SFMT_N32 {
+            self.gen_all();
+            self.0.idx = 0;
+        }
+        let u = self.get();
+        self.0.idx += 1;
+        u
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::io::Read;
+
+    fn read_answer() -> Result<Vec<u32>, std::io::Error> {
+        let mut f = std::fs::File::open("SFMT_19937.txt")?;
+        let mut buf = String::new();
+        f.read_to_string(&mut buf)?;
+        Ok(buf.split(" ")
+            .map(|s| s.trim().parse().expect("Failed to parse into u32"))
+            .collect())
+    }
+
+    #[test]
+    fn gen_u32() {
+        let ans = read_answer().expect("Failed to load answers");
+        let mut sfmt = SFMT::new(1234);
+        for (t, val) in ans.into_iter().enumerate() {
+            let r = sfmt.next_u32();
+            println!("[{}] gen = {}, ans = {}", t, r, val);
+            assert_eq!(r, val);
+        }
     }
 }
