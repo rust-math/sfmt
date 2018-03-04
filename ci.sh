49ee8b706598488e31b2c243159938bf4b8bc9af
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-03-05 04:46:20 +0900

    Add benchmark test for rsfmt (import from #3)

--- ci.sh
+++ ci.sh
@@ -4,5 +4,8 @@ set -ex
 cargo test
 cd sfmt-sys && cargo test && cd -
 if [[ $TRAVIS_RUST_VERSION = nightly ]]; then
-  cd rsfmt && cargo test && cd -
+  cd rsfmt
+  cargo test
+  cargo bench
+  cd -
 fi
