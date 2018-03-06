5a548423a33fab83d09cc2d4add0cc4145368fdd
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-03-06 16:37:52 +0900

    Add clang-format

--- check/check.sh
+++ check/check.sh
@@ -9,5 +9,6 @@ arc="${dir}.tar.gz"
 wget http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/SFMT/${arc}
 tar xvf $arc
 rm $arc
+clang-format -i ${dir}/*.h ${dir}/*.c
 g++ -msse2 -DHAVE_SSE2 mm_recursion.c && ./a.out > mm_recursion.txt
 g++ -msse2 -DHAVE_SSE2 init.c SFMT-src-1.5.1/SFMT.c && ./a.out > init1234.txt
